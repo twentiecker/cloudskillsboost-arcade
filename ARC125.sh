379c2d06e4365600efdbdc9c2527747d6adf9d76
twentiecker <twentiecker@gmail.com>
2025-08-02 21:07:19 +0700

    first commit

--- ARC125.sh
+++ ARC125.sh
@@ -1,13 +1,3 @@
-# Fetch zone and region
-ZONE=$(gcloud compute project-info describe \
-  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-REGION=$(gcloud compute project-info describe \
-  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
-PROJECT_ID=$(gcloud config get-value project)
-
-# wget https://github.com/quiccklabs/Labs_solutions/blob/master/Use%20APIs%20to%20Work%20with%20Cloud%20Storage%20Challenge%20Lab/world.jpeg
-
-
 # Task 1. Create two Cloud Storage buckets
 # Create bucket1.json
 cat > bucket1.json <<EOF
@@ -17,10 +7,8 @@ cat > bucket1.json <<EOF
    "storageClass": "multi_regional"
 }
 EOF
-
 # Create bucket1
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data-binary @bucket1.json "https://storage.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
-
 # Create bucket2.json
 cat > bucket2.json <<EOF
 {  
@@ -29,19 +17,15 @@ cat > bucket2.json <<EOF
    "storageClass": "multi_regional"
 }
 EOF
-
 # Create bucket2
-# Download image from the lab and upload it to the cloudshell
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data-binary @bucket2.json "https://storage.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
 
-
 # Task 2. Upload an image file to a Cloud Storage Bucket
+# Download image from the lab and upload it to the cloudshell
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: image/jpeg" --data-binary @world.jpeg "https://storage.googleapis.com/upload/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o?uploadType=media&name=quicklab.jpeg"
-
 
 # Task 3. Copy a file to another bucket
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data '{"destination": "$DEVSHELL_PROJECT_ID-bucket-2"}' "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg/copyTo/b/$DEVSHELL_PROJECT_ID-bucket-2/o/quicklab.jpeg"
-
 
 # Task 4. Make an object (file) publicly accessible
 cat > public_access.json <<EOF
@@ -50,28 +34,10 @@ cat > public_access.json <<EOF
   "role": "READER"
 }
 EOF
-
 curl -X POST --data-binary @public_access.json -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg/acl"
 
-
 # Task 5. Delete the object file and a Cloud Storage bucket (Bucket 1)
+# Delete file
 curl -X DELETE -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg"
-
 # Delete bucket1
 curl -X DELETE -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1"
-
-
-
-
-
-cat > bucket1.json <<EOF
-{  
-   "name": "$DEVSHELL_PROJECT_ID-bucket-1",
-   "location": "us",
-   "storageClass": "multi_regional"
-}
-EOF
-
-
-# Create bucket1
-curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data-binary @bucket1.json "https://storage.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
