13d37662b094a4f6d2734de8f552c466425395f2
twentiecker <twentiecker@gmail.com>
2025-08-02 21:20:20 +0700

    first commit

--- ARC111.sh
+++ ARC111.sh
@@ -1,3 +1,26 @@
+#form 2
+export Bucket_1=
+export Bucket_2=
+export Bucket_3=
+export USER_EMAIL=
+
+# Task 1
+# Create a Cloud storage bucket
+gsutil mb -c nearline gs://$Bucket_1
+
+# Task 2
+# Publish Cloud Storage files to web
+gcloud alpha storage buckets update gs://$Bucket_2 --no-uniform-bucket-level-access
+gsutil acl ch -u $USER_EMAIL:OWNER gs://$Bucket_2
+gsutil rm gs://$Bucket_2/sample.txt
+echo "subscibe to quicklab" > sample.txt
+gsutil cp sample.txt gs://$Bucket_2
+gsutil acl ch -u allUsers:R gs://$Bucket_2/sample.txt
+
+# Task 3
+# Add labels to cloud storage bucket
+gcloud storage buckets update gs://$Bucket_3 --update-labels=key=value
+
 #form 3
 export Bucket_1=
 export Bucket_2=
