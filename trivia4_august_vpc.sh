c210f022120946712e08cc53fd20b72a66a0e4f8
twentiecker <twentiecker@gmail.com>
2025-08-15 20:47:35 +0700

    first commit

--- trivia4_august_vpc.sh
+++ trivia4_august_vpc.sh
@@ -1,18 +1,12 @@
+# Challenge scenario
+# As a network administrator, you have been assigned with the responsibility of connecting two Virtual Private Clouds (VPCs) workspace_vpc and private_vpc in your project. This peering connection will establish a direct and secure communication pathway between the resources residing in each VPC, allowing them to interact seamlessly with each other.
+# Your task is :
+# Create Peering connection workspace-vpc with private-vpc
+# Create Peering connection private-vpc with workspace-vpc
 
-gcloud auth list
-
-export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-
-export PROJECT_ID=$(gcloud config get-value project)
-
-gcloud config set compute/zone "$ZONE"
-
-gcloud compute networks create workspace-vpc --subnet-mode=custom
-
-gcloud compute networks create private-vpc --subnet-mode=custom
-
+# create networks peering
 gcloud compute networks peerings create workspace-to-private --network=workspace-vpc --peer-network=private-vpc --auto-create-routes
-
 gcloud compute networks peerings create private-to-workspace --network=private-vpc --peer-network=workspace-vpc --auto-create-routes
 
-gcloud compute ssh workspace-vm --project="$PROJECT_ID" --zone="$ZONE"
+# i got warning like this
+# WARNING: Flag --auto-create-routes is deprecated and will be removed in a future release.
